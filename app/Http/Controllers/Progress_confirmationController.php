1ae10e70e9341e5ca5975e58c3704910ccaf4ff4
KouseiImai <kouseiimai16340@gmail.com>
2021-01-13 14:26:28 +0900

    creating a Progress_confirmationController file

---
+++ app/Http/Controllers/Progress_confirmationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Lot_number;
+use App\Models\Process_data;
+use App\Models\Production_item;
+
+class Progress_confirmationController extends Controller
+{
+    public function index(Request $request)
+    {
+        //
+    }
+}
