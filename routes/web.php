90d0472a21ba3d907addc83f0bb5b0ad7db1009f
KouseiImai <kouseiimai16340@gmail.com>
2021-01-19 16:48:33 +0900

    Routing description

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Process_registrationController;
 use App\Http\Controllers\Progress_confirmationController;
+use App\Http\Controllers\Daily_reportController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,5 @@ Route::get('/process/new', [Process_registrationController::class, 'new']);
 Route::post('/process/create', [Process_registrationController::class, 'create']);
 
 Route::get('/progress/index', [Progress_confirmationController::class, 'index']);
+
+Route::get('/report/new', [Daily_reportController::class, 'new']);
