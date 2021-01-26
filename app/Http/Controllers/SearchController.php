72536101e5bc6abcb3885f3cbd90cc6a269c419c
KouseiImai <kouseiimai16340@gmail.com>
2021-01-26 22:30:08 +0900

    Added description for passing item and lot number data on the search input screen

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Production_item;
+use App\Models\Lot_number;
 
 class SearchController extends Controller
 {
     public function index (Request $request)
     {
-        return view('search.index');
+        $item_names = Production_item::all();
+        $lot_numbers = Lot_number::all();
+
+        return view('search.index',compact('item_names','lot_numbers'));
     }
 }
